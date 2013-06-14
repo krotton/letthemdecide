ab9ca19bf9256788f9796acf749d520e7996e1ae
Krotton <krotton@gmail.com>
2013-06-14 02:45:08 +0200

    Configuration file and database connection.

--- src/index.php
+++ src/index.php
@@ -1,6 +1,24 @@
 <?php
     require_once '../vendor/autoload.php';
 
+    phpinfo();
+
+    # Parse the config.
+    $config = parse_ini_file('../config.ini', true);
+    
+    if ($config === FALSE)
+        die('Config file not found or unparseable!');
+
+    # Connect to the database.
+    try
+    {
+        $db = new PDO($config['database']['dsn']);
+    }
+    catch (PDOException $e)
+    {
+        die("Could not connect to the database! Error: {$e->getMessage()}");
+    }
+
     # Initialize templating.
     $twig = new Twig_Environment(
         new Twig_Loader_Filesystem('templates')
